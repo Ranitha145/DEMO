bf2b2b362b851e8bde4c6c7f60b5247082efcb6f
ranitha <ranitha@CEI3094.CEIIND.COM>
2019-08-26 20:29:59 +0530

    added

---
+++ Test Suites/Demo Test Suite.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0d7b1184-4f07-48d8-a2f9-7e0903a1fdb0</testSuiteGuid>
+   <testCaseLink>
+      <guid>46a1a576-3415-4d7c-86c9-849a3a55ace7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>55bafb07-346b-4b0a-88a5-e7560952333d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyBrokenLinks_Fail_Scenario</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6a0798f-f3ba-44c9-a45b-5f8254df9fb8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyBrokenLinks_Pass_Scenario</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
